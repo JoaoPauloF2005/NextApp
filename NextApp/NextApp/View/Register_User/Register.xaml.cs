95e3a2eca00d592bffe578c2dab53dfc52c8fbe1
JoaoPauloF2005 <hypergamerfrr@gmail.com>
2023-05-03 15:08:00 -0300

    Adicionando a tela de Registro de novo usuario

---
+++ NextApp/NextApp/View/Register_User/Register.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NextApp.View.Register_User
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Register : ContentPage
+    {
+        public Register()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+        }
+    }
+}
