db4b8388c5a2a4633a28e50bd5a2e9fe66f3eb60
JoaoPauloF2005 <hypergamerfrr@gmail.com>
2023-05-03 15:07:33 -0300

    Atualizando a pagina de Login e seus estilos

--- NextApp/NextApp/View/Login/Login.xaml.cs
+++ NextApp/NextApp/View/Login/Login.xaml.cs
@@ -20,11 +20,27 @@ namespace NextApp.View.Login
 
         private void ImageButton_Clicked(object sender, EventArgs e)
         {
-            App.Current.MainPage = new NavigationPage(new View.Pagina_Inicial.Inicio());
 
         }
 
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(txt_cpf.Text=="555.648.808-89" && txt_senha.Text == "123")
+            {
 
+                Navigation.PushAsync(new View.Home.Inicio());
+            }
+            else
+            {
+                DisplayAlert("Ops...", "CPF ou Senha incorreta!", "Ok");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.Register_User.Register());
+
+        }
     }
 
 
