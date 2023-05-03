c987fca1d4b11ab60ab8b9a11a5f31285280b1a9
JoaoPauloF2005 <hypergamerfrr@gmail.com>
2023-05-03 15:07:16 -0300

    Adicionando arquivos referente ao Inicio

---
+++ NextApp/NextApp/View/Home/Inicio.xaml.cs
@@ -0,0 +1,23 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NextApp.View.Pagina_Inicial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
+*/
