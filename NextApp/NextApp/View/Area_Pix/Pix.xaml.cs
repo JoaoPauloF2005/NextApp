ed3cd3cc791c0c04f62a1adc1ba33830d00da749
joao <hypergamerfrr@gmail.com>
2023-06-17 16:12:59 -0300

    Criando a tela da area de pix

---
+++ NextApp/NextApp/View/Area_Pix/Pix.xaml.cs
@@ -0,0 +1,34 @@
+﻿using NextApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace NextApp.View.Area_Pix
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Pix : ContentPage
+	{
+		public Pix ()
+		{
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private void OnLabelTapped(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new View.Area_Pix.Pix());
+        }
+
+    }
+}
